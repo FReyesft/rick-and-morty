f3d16677f96b525705e1d2b340d1fb626b096520
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-11-27 15:39:16 -0500

    add header component

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+export default function Header() {
+	return (
+		<nav className="backdrop-blur-sm bg-white/10 rounded-md max-w-screen-md w-screen p-5">
+			<ul className="flex justify-between text-xl items-center">
+				<li>
+					<Image 
+						alt="Icon RickAndMorty"
+						width={150}
+						height={150}
+						src={'https://www.themoviedb.org/t/p/original/kCyZc7mvqHdeUGpguSGxsWPxqpI.png'}>
+					</Image>
+				</li>
+				<li>By <span className="text-2xl text-blue-400 font-bold">FReyeSFT</span></li>
+			</ul>
+		</nav>
+	) 
+}
